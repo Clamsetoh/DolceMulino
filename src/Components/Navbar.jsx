633efd82f82283d9f175e48f8d856178ce223418
Clamsetoh <cesarluis1808@gmail.com>
2023-05-30 20:24:06 -0500

    Primer commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Style from 'styled-components'
+
+function Navbar() {
+  return (
+    <>
+    <NavContainer>
+        <h2>Navbar <span>responsive</span></h2>
+    </NavContainer>
+    </>
+  )
+}
+
+export default Navbar
+
+const NavContainer = styled.nav`
+
+`
